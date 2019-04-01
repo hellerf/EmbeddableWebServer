0e64041ebc9c966e8447a61c5b2cced974e4278c
hellerf <forrest@forrestheller.com>
2019-04-01 23:27:34 +0800

    Just put the authors directly in the license for now. If it gets to more than 5 contributors we can revisit this.

--- EmbeddableWebServer.h
+++ EmbeddableWebServer.h
@@ -1,4 +1,4 @@
-/* Copyright (c) Forrest Heller and contributors - All rights reserved. Released under the BSD 2-clause license:
+/* Copyright (c) 2016, 2019 Forrest Heller, Martin Pulec, Daniel Barry - All rights reserved. Released under the BSD 2-clause license:
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
